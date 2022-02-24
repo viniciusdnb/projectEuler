39ef4327beb80e4eeabb5dbeb0db09a3b44963ce
Vinicius Henrique costa da silva <71468843+viniciusdnb@users.noreply.github.com>
2022-02-24 16:57:13 -0300

    Create numerosMultipo3e5.js

---
+++ numerosMultipo3e5.js
@@ -0,0 +1,62 @@
+function arrMultiple(numberNature, numberListMultiple)
+{
+  var arr = [];
+  for(let i = 0; i < numberListMultiple; i++)
+  {
+    if(i % numberNature == 0)
+    {
+      arr[i] = i;
+    }else
+    {
+      arr[i] = 0;
+    }
+  }
+  return filterArr(arr);
+}
+
+function filterArr(arr)
+{
+  var x = arr.filter(function(a){return a})
+  return x;
+}
+
+function multipleComun(arr1, arr2)
+{
+  var y = [];
+  for(let i = 0; i < arr1.length; i++)
+  {
+    for(let b = 0; b < arr1.length; b++)
+    {
+      if(arr1[i] == arr2[b])
+      {
+        y[b] = arr2[b];
+      }      
+    }    
+  }
+  return filterArr(y);
+}
+
+function sumArray(arr)
+{
+  var sum = arr.reduce(function(sum,i){return sum+i});
+  return sum;
+}
+
+var list = 1000;
+var number1 = 3;
+var number2 = 5;
+
+var arr1 = arrMultiple(number1,list);
+var arr2 = arrMultiple(number2,list);
+
+var mc = multipleComun(arr1, arr2);
+
+console.log("soma dos multiplos comum " + sumArray(mc));
+console.log("soma de todos os multiplos " + sumArray(arr1)+sumArray(arr2));
+console.log("resultado de soma dos multipos menos o multiplos comum "+ (sumArray(arr1)+sumArray(arr2) - sumArray(mc)));
+
+
+
+
+
+
