f008bf48e66b4d268a6491e0e2516503dfe2d2a0
Vinicius Henrique costa da silva <71468843+viniciusdnb@users.noreply.github.com>
2022-03-01 15:38:28 -0300

    Update 10001stprime.js
    melhorando o tempo de processamento

--- 10001stprime.js
+++ 10001stprime.js
@@ -3,61 +3,52 @@ function isPrime(numero, max)
   var result = 0;
   
 
-  for(let i = 0; i < max; i++)
+  for(let i = 0; i <= max; i++)
   {
      if(numero % i == 0)
      {
         result++;
-              
-     }
+              if(result > 2)
+              {
+                return false;
+              }
+         }
   }  
-  if(result > 2)
-  {
-    return false;
-  }else{
-    return true;
-  }
+  return true
   
 }
    
-function teste(index)
+function gerarArr(index)
 {
   var arr = [];
   var max = index * 100;
   var key = 0;
     var p = 0;
     var l = 0
-    console.log("iniciando o calculo");
-  for(let i = 0; i < max; i++)
+   // console.log("iniciando o calculo");
+  for(let i = 2; i < max; i++)
   {
-    if(isPrime(i, max))
+    if(isPrime(i, i))
     {
       //console.log(i);
       arr[key] = i;
       key++;
+
+        if(arr.length == index)
+        {
+            //console.log("100 % calculado")
+            return arr;
+        }   
     }
-      //console.log(i / max);
-      l = (i/ max) * 100
-      if(l == p)
-      {
-          console.log("aguarde calculo em " + (p) + "%");
-          p = p + 10;
-      }
-
-      //console.log(p);
-   /* if(i / max == 0.5 )
-     {
-         console.log(" aguarde calculo em 50%");
-     }
-
-      if(i / max == 0.80)
-      {
-          console.log("aguarde calculo em  80%")
-      }*/
+     
   }
-    console.log("100 % calculado")
-  return arr[index];
+    
   
 }
 
-console.log(teste(10001))
+function retornaPosicao(posicao)
+{
+    var arr = gerarArr(posicao);
+    return arr[posicao -1];
+}
+
