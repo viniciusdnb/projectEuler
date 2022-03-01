fcfba65a3ff381f4c8311f38f29ce0134efcbd1e
Vinicius Henrique costa da silva <71468843+viniciusdnb@users.noreply.github.com>
2022-03-01 14:09:36 -0300

    criação de loading

--- 10001stprime.js
+++ 10001stprime.js
@@ -20,21 +20,43 @@ function isPrime(numero, max)
   
 }
    
-function teste()
+function teste(index)
 {
   var arr = [];
-  var max = 150000;
+  var max = index * 100;
   var key = 0;
+    var p = 0;
+    var l = 0
+    console.log("iniciando o calculo");
   for(let i = 0; i < max; i++)
   {
     if(isPrime(i, max))
     {
-      console.log(i);
+      //console.log(i);
       arr[key] = i;
       key++;
     }
+      //console.log(i / max);
+      l = (i/ max) * 100
+      if(l == p)
+      {
+          console.log("aguarde calculo em " + (p) + "%");
+          p = p + 10;
+      }
+
+      //console.log(p);
+   /* if(i / max == 0.5 )
+     {
+         console.log(" aguarde calculo em 50%");
+     }
+
+      if(i / max == 0.80)
+      {
+          console.log("aguarde calculo em  80%")
+      }*/
   }
-  return arr;
+    console.log("100 % calculado")
+  return arr[index];
   
 }
 
