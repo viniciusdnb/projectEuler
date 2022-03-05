d4c9f3aac2d014c1c9ddec4e8dba0d4c6f33af57
Vinicius Henrique costa da silva <71468843+viniciusdnb@users.noreply.github.com>
2022-03-05 15:43:37 -0300

    logica funcuina até 7 numeros

--- Largestproductinaseries.js
+++ Largestproductinaseries.js
@@ -1,7 +1,40 @@
 var numbers = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
 
 var arrNumbers = numbers.split("");
+function numerosEsquerda(arrNumbers)
+{
+	var arrayNumerosEsquerda = [];
+	var indexEsquerda = 0;
 
+	for(let i = 0; i < arrNumbers.length; i++)
+	{
+		if(arrNumbers[i] == 9)
+		{
+			var contadorEsquerda = 7;
+			var posicaoEsquerda = i;
+			var numerosEncontradoEsquerda = "";
+			
+			while(contadorEsquerda > 0)
+			{
+				if(arrNumbers[posicaoEsquerda] == 0)
+				{
+					break;
+				}
+
+				numerosEncontradoEsquerda += arrNumbers[posicaoEsquerda];
+				contadorEsquerda--;
+				posicaoEsquerda--;
+			}
+
+			arrayNumerosEsquerda[indexEsquerda] = numerosEncontradoEsquerda;
+			indexEsquerda++;
+			contadorEsquerda--
+		}
+
+	}
+
+	return arrayNumerosEsquerda;
+}
 function numerosDireita(arrNumbers)
 {
     var arrayNumerosDireita = [];
@@ -9,14 +42,14 @@ function numerosDireita(arrNumbers)
 
     for(let i = 0; i < arrNumbers.length; i++)
     {
-        console.log(arrNumbers[i])
+        //console.log(arrNumbers[i])
         if(arrNumbers[i] == 9)
         {
             var contadorDireita = 0;
             var posicaoDireita = i;
             var numerosEncontradoDireita= "";
         
-            while(contadorDireita < 13)
+            while(contadorDireita < 7)
             { 
                 if(arrNumbers[posicaoDireita] == 0)
                 {
@@ -36,3 +69,50 @@ function numerosDireita(arrNumbers)
 
     return arrayNumerosDireita;
 }
+
+function mult(arrNumbers)
+{
+	var arr = arrNumbers.split("");
+	var total = arr.reduce((a,b)=>
+	{
+		return  a * b ;
+	})
+
+	return total;
+}
+
+function maxSequence(arrNumbers)
+{
+	var direita = numerosDireita(arrNumbers);
+	var esquerda = numerosEsquerda(arrNumbers);
+	var arrD = [];
+	for(let i = 0; i < direita.length; i++)
+	{
+
+		arrD[i] = mult(direita[i]);
+	}
+
+	return arrD;
+}
+
+function maxProduto(arrNumbers)
+{
+	var arr = arrNumbers[0];
+	var contador = 0;
+	var length = arrNumbers.length;
+
+	while(length--)
+	{
+		if(arr < arrNumbers[contador])
+		{
+			arr = arrNumbers[contador];
+		}
+		contador++;
+	}
+
+	return arr;
+
+}
+var valor = maxSequence(arrNumbers);
+
+console.log(maxProduto(valor))
